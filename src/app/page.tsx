0182ba5b67a98794907e3d066c5544659d12f623
Asmar Tiba <asmartiba@hotmail.com>
2023-10-31 01:20:20 +0100

    מוכן להתחיל

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,18 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-export default function Home() {
+const Home = () => {
+
   return (
+
     <main className={styles.main}>
         
-       
       <h1>זה עובד</h1>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <h2>אנחנו כאן הם שם</h2>
 
-      <h2>אנחנו כאן הם שם
-
-      </h2>
     </main>
   )
 }
+
+export default Home;
