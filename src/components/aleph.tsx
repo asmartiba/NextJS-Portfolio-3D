b4584f46d4cd82cf5afa43bf3c5f33bc6c7ca763
Asmar Tiba <asmartiba@hotmail.com>
2023-11-23 11:11:16 +0100

    מה לעשות?

--- src/components/aleph.tsx
+++ src/components/aleph.tsx
@@ -84,7 +84,6 @@ const Aleph = () => {
     
       return (
           <>
-              <div className={styles.cGroup}>
                 <div className={styles.container3D} >
                   <Canvas>
                     <pointLight position={[10, 10, 10]} />
@@ -94,71 +93,9 @@ const Aleph = () => {
                     </mesh>
                     <OrbitControls target={controlsTarget} enableDamping dampingFactor={0.25} rotateSpeed={0.5} enableZoom={false}/>
                   </Canvas>
-                  <h2>Container is here</h2>
-
                 </div>
-  
-                <div className={styles.containerText}>
-                  <h2><span style={{fontSize:'120%'}}>F</span>ront-end & <span style={{fontSize:'120%'}}>C</span>reative web development</h2>
-                  <h3>  
-                    One of my favourite spaces in the world of technology. Learning about interfaces and design challenges.
-                    I like to work using <span style={{color: "#f7df1e"}}>Javascript</span>, <span style={{color: "#f16524"}}>HTML</span>,  <span style={{color: "#2862e9"}}>CSS</span> and  <span style={{color: "#c66394"}}>SASS</span> using <span style={{color: "#61dbfb"}}>React framework.</span>
-                  </h3>
-  
-                  <h3>Recently I have been using <span style={{color: "#c4002b"}}>Angular</span>, <span style={{color: "#9d5efe"}}>Vite</span> and <span style={{color: "#5fcec9"}}>NextJS</span>, thus I'm becoming more familiar with <span style={{color: "#51dbfb"}}>Typescript</span> as a main programming language for advanced queries.</h3>
-                </div>
-              </div>
-  
-              <div className={styles.cGroup}>
-                      <div className={styles.containerText}>
-                          <h2><span style={{color: '#0173aa'}}>WordPress</span>, <span style={{color: '#11b3aa'}}>Netilify </span> & <span style={{color: '#6626ab'}}>GatsbyJS</span></h2>
-                          <h3> 
-                              in content management system CMS development. I learnt about using WordPress as a headless CMS. Hosting it as a static site via Gatsby and Netilify. 
-                              In this subject I learnt working with advanced JavaScript and connecting front and back-end via API by writing GraphQL queries. 
-                          </h3>
-                      </div>
-                      {/* <img src="./../images/title/wpgatsby.png" alt="icons" width={200} height={200} />
-                      <Image 
-                          src="/images/title/wpgatsby.png"
-                          alt="icons"
-                          width={200}
-                          height={200}
-                      /> */}
-                </div>
-  
-                <div className={styles.cGroup}>
-  
-                    {/* <Image 
-                          src="../images/title/Responsive.png"
-                          alt="icon"
-                          className={styles.imageM}
-                      /> */}
-                    <div className={styles.containerText}>
-                      <h2><span style={{color: '#11b3aa'}}>Responsive</span></h2>
-                      <h3> 
-                          Designing a functional website compatible with different screen sizes and resolutions can be challenging. 
-                          My current approach for implementing wireframes would be writing media queries   
-                      </h3>
-                    </div>
-               
-                </div>
-  
-                <div className={styles.cGroup}>
-                  
-                      <div className={styles.containerText}>
-                          <h2><span style={{color: '#0173aa'}}>API development </span></h2>
-                          <h3> 
-                              Building a robust and efficient API to connect and interact with data sources, enabling communication between front-end and back-end of a web application.
-                              In <span style={{color: "#be73d8"}}>ASP.NET</span>, <span style={{color: "#f7df1e"}}>JSON</span> serialization plays a role in making data accesible via web applications. 
-                              Through the magic of serialization, ASP.NET APIs convert .NET objects into JSON format, making data exchange between the front and back-end seamless and efficient.
-                          </h3>
-                      </div>
-                      <video autoPlay loop className={styles.imageS}>
-                        <source src="./../images/webm/api.webm" type="video/webm" />
-                      </video>
-                  </div>
-  
-        
+              
+                
           </>
     )};
   
